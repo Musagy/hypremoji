545f8c056dc7c793df56008ef3f79c3eb7d44332
Musagy <diegomusagy@gmail.com>
2025-07-16 01:29:05 -0500

    fix: emoji clipboard handling now works properly with wl-copy

--- src/utils/clipboard_manager.rs
+++ src/utils/clipboard_manager.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, process::Command, rc::Rc, thread, time::Duration};
 
-use arboard::Clipboard;
+// use arboard::Clipboard; // <-- If don't work, try with that
 use chrono::Utc;
 
 use crate::utils::get_last_client;
@@ -39,21 +39,35 @@ pub fn get_clipboard_manager() -> ClipboardManager {
 }
 
 fn send_emoji(emoji: &str, window_id: &str) {
-    let mut clipboard = Clipboard::new().expect("No se pudo acceder al clipboard");
+    // // If don't work, try with that
+    // let mut clipboard = Clipboard::new().expect("No se pudo acceder al clipboard");
 
     // 1. Try to save the original clipboard content
     let original_clipboard_content = save_original_clipboard_content();
-
+    
     // 2. Set the emoji to the clipboard
-    clipboard
-        .set_text(emoji.to_string())
-        .expect("No se pudo poner el emoji en el clipboard");
+    let emoji_status = Command::new("wl-copy")
+        .arg("--type")
+        .arg("text/plain")
+        .arg("--")
+        .arg(emoji)
+        .status();
+
+    if let Err(e) = emoji_status {
+        eprintln!("Failed to copy emoji to clipboard: {}", e);
+    }
+    
+    // // If don't work, try with that
+    // clipboard
+    //     .set_text(emoji.to_string())
+    //     .expect("No se pudo poner el emoji en el clipboard");
 
     // 3. Insert the emoji into the previously focused window
     let command_str = format!(
         "hyprctl dispatch sendshortcut CONTROL, V, address:{}",
         window_id
     );
+
     Command::new("sh")
         .arg("-c")
         .arg(&command_str)
@@ -77,12 +91,15 @@ fn save_original_clipboard_content() -> OriginalClipboardContent {
 
     let stdout = String::from_utf8_lossy(&types_output.stdout);
 
+    
     if stdout.contains("Nothing is copied") || stdout.trim().is_empty() {
         return OriginalClipboardContent {
             content: None,
             mime_type: "empty".to_string(),
         };
     }
+    // stdout.to_string();
+    // println!("{}", stdout.to_string());
 
     let mime_type = if stdout.contains("image/png") {
         "image/png"
